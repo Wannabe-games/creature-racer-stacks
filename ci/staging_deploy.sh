c67dcc8e8f480b4c272722f81e05ab2756c8521a
pki-acs <pki-acs@ex.com.pl>
2022-10-30 14:45:02 +0100

    staging env automation

---
+++ ci/staging_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# This should be executed on staging machine
+
+PROJECT_ROOT=$(readlink -f $(dirname $0)/..)
+PROTOCOL_DIR=$PROJECT_ROOT/protocol
+SESSION_NAME=creatures_staging
+
+cd $PROJECT_ROOT
+WANT_RESET=0
+git remote update
+if git status -uno | grep -q behind; then
+    WANT_RESET=1
+fi
+
+RUNNING_PID=$(tmux ls -f session-name=$SESSION_NAME -F '#{pid}')
+if [ "$RUNNING_PID" != "" ]; then
+    if [ $WANT_RESET -eq 1 ]; then
+        tmux send-keys -t ${SESSION_NAME}.0 C-c
+        while [ "$RUNNING_PID" != "" ]; do
+            sleep 1
+            RUNNING_PID=$(tmux ls -f session-name=$SESSION_NAME -F '#{pid}')
+        done
+        git pull
+    else
+        exit 0
+    fi
+fi
+
+tmux new-session -d -c $PROTOCOL_DIR -s $SESSION_NAME \
+     'clarinet integrate -c'
