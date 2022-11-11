07c9e34a6e692f319b43f9baa4aa589d357852a5
pki-acs <pki-acs@ex.com.pl>
2022-11-11 11:22:56 +0100

    be more dry in tests
    - operator setting factored out to utility file

--- protocol/tests/creature-racer-referral-nft_test.ts
+++ protocol/tests/creature-racer-referral-nft_test.ts
@@ -1,9 +1,11 @@
 
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.4/index.ts';
-import { assertEquals, fail } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
-import { pubKeyfromPrivKey, makeRandomPrivKey,
+import { makeRandomPrivKey,
          privateKeyToString,
          getAddressFromPrivateKey } from 'https://esm.sh/@stacks/transactions';
+import { assertEquals, fail } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+import { setOperator } from './utils/admin.ts';
 
 function mintToken(chain: Chain, user: Account, refCode: string,
                    caller: Account) {
@@ -15,20 +17,6 @@ function mintToken(chain: Chain, user: Account, refCode: string,
   ]);
 }
 
-function setOperator(chain: Chain, deployer: Account, 
-                     operator: Account) {
-  let secretKey = 'd655b2523bcd65e34889725c73064feb17ceb796831c0e111ba1a552b0f31b3901';
-  let publicKey = pubKeyfromPrivKey(secretKey);
-  let block = chain.mineBlock([
-    Tx.contractCall('creature-racer-admin',
-                    'set-operator', 
-                    [types.some(types.principal(operator.address)),
-                     types.some(types.buff(publicKey))],
-                    deployer.address)
-  ]);
-  assertEquals(block.receipts.length, 1);
-  assertEquals(block.receipts[0].result, '(ok true)');
-}
 
 function getInvitationsByInvitee(chain: Chain,
                                    user: Account,
