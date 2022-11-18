2cd5fe7b4ef7413981cfa8a40c778de778bebbd5
pki-acs <pki-acs@ex.com.pl>
2022-11-18 07:17:31 +0100

    rNFT example updated for new testnet
    - API endpoint switched from devnet to testnet
    - referral code data type updated to utf8
    - change minting wallet to wallet8 from operator

--- examples/rnft-mint/rnft-mint.js
+++ examples/rnft-mint/rnft-mint.js
@@ -2,18 +2,18 @@ const {
   makeContractCall,
   broadcastTransaction,
   standardPrincipalCV,
-  stringAsciiCV,
+  stringUtf8CV,
 } = require("@stacks/transactions");
 const { StacksTestnet } = require("@stacks/network");
 
 
 async function main() {
-  const apiEndpoint = 'http://localhost:3999';
+  const apiEndpoint = 'https://stacksapi-testnet.wannabe.games';
   const network = new StacksTestnet( { url: apiEndpoint } );
 
   // Get a valid parameters from your protocol/settings/Devnet.toml
   const deployerAddress = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
-  const operatorKey = '753b7cc01a1a2e86221266a154af739463fce51219d97e4f856cd7200c3bd2a601';
+  const senderKey = '6a1a754ba863d7bab14adbbc3f8ebb090af9e871ace621d3e5ab634e1422885e01';
   
   const receiverAddress = 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5';
 
@@ -23,8 +23,8 @@ async function main() {
     functionName: 'mint',
     fee: 500,
     functionArgs: [ standardPrincipalCV(receiverAddress),
-                    stringAsciiCV('ABCDEF') ],
-    senderKey: operatorKey,
+                    stringUtf8CV('ABCDEF') ],
+    senderKey: senderKey,
     validateWithAbi: true,
     network,
   };
