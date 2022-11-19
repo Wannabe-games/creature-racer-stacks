cad09ec13cc6824336da92e27b170879cf1a3191
pki-acs <pki-acs@ex.com.pl>
2022-11-20 00:22:45 +0100

    Argument signing related updates
    - added message signing code
    - dropped requirement to explicitly set
      operator's public key

--- protocol/tests/creature-racer-admin_test.ts
+++ protocol/tests/creature-racer-admin_test.ts
@@ -1,26 +1,127 @@
 
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.4/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+import { makeSignatures, setOperator } from './utils/admin.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that it verifies correct signatures",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+      let owner = accounts.get('deployer')!;
+      let operator = accounts.get('wallet_1')!
+      let opseckey = '7287ba251d44a4d3fd9276c88ce34c5c52a038955511cccaf77e61068649c17801';
+      let sender = accounts.get('wallet_2')!;
+      let senderseckey = '530d9f61984c888536871c6573073bdfc0058896dc1adfe9a6a10dfacadc209101';
 
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
+      setOperator(chain, owner, operator);
+
+      const sigs = makeSignatures(opseckey, senderseckey, 
+                                  // call arguments follow
+                                  500, 531101);
+      let block = chain.mineBlock([
+        Tx.contractCall('creature-racer-admin',
+                        'verify-signature',
+                        [types.buff(sigs.operatorSignature),
+                         types.buff(sigs.senderSignature),
+                         types.list([types.uint(500),
+                                     types.uint(531101)])],
+                        sender.address
+                       )
+      ]);
+      assertEquals(block.height, 3);
+      assertEquals(block.receipts.length, 1);
+      assertEquals(block.receipts[0].result, '(ok true)');
+    }
+});
+
+
+Clarinet.test({
+    name: "Ensure that it rejects invalid signatures",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+      let owner = accounts.get('deployer')!;
+      let operator = accounts.get('wallet_1')!
+      let opseckey = '7287ba251d44a4d3fd9276c88ce34c5c52a038955511cccaf77e61068649c17801';
+      let sender = accounts.get('wallet_2')!;
+      let senderseckey = '530d9f61984c888536871c6573073bdfc0058896dc1adfe9a6a10dfacadc209101';
+
+      setOperator(chain, owner, operator);
+
+      const sigs = makeSignatures(opseckey, senderseckey, 
+                                  // call arguments follow
+                                  500, 531101);
+      let block = chain.mineBlock([
+        Tx.contractCall('creature-racer-admin',
+                        'verify-signature',
+                        [types.buff(sigs.operatorSignature),
+                         types.buff(sigs.senderSignature),
+                         types.list([types.uint(500),
+                                     types.uint(531100)])],
+                        sender.address
+                       )
+      ]);
+      assertEquals(block.height, 3);
+      assertEquals(block.receipts.length, 1);
+      assertEquals(block.receipts[0].result, '(err u1002)');
+    }
+});
+
+
+
+Clarinet.test({
+    name: "Ensure that it verifies sender principal",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+      let owner = accounts.get('deployer')!;
+      let operator = accounts.get('wallet_1')!
+      let opseckey = '7287ba251d44a4d3fd9276c88ce34c5c52a038955511cccaf77e61068649c17801';
+      let sender = accounts.get('wallet_2')!;
+      let senderseckey = '530d9f61984c888536871c6573073bdfc0058896dc1adfe9a6a10dfacadc209101';
+
+      setOperator(chain, owner, operator);
+
+      const sigs = makeSignatures(opseckey, senderseckey, 
+                                  // call arguments follow
+                                  500, 531101);
+      let block = chain.mineBlock([
+        Tx.contractCall('creature-racer-admin',
+                        'verify-signature',
+                        [types.buff(sigs.operatorSignature),
+                         types.buff(sigs.senderSignature),
+                         types.list([types.uint(500),
+                                     types.uint(531101)])],
+                        accounts.get('wallet_3')!.address
+                       )
+      ]);
+      assertEquals(block.height, 3);
+      assertEquals(block.receipts.length, 1);
+      assertEquals(block.receipts[0].result, '(err u1002)');
+    }
+});
+
+Clarinet.test({
+    name: "Ensure that it verifies operator principal",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+      let owner = accounts.get('deployer')!;
+      let operator = accounts.get('wallet_1')!
+      let opseckey = '7287ba251d44a4d3fd9276c88ce34c5c52a038955511cccaf77e61068649c17801';
+      let sender = accounts.get('wallet_2')!;
+      let senderseckey = '530d9f61984c888536871c6573073bdfc0058896dc1adfe9a6a10dfacadc209101';
+
+      setOperator(chain, owner, accounts.get('wallet_5')!);
+
+      const sigs = makeSignatures(opseckey, senderseckey, 
+                                  // call arguments follow
+                                  500, 531101);
+      let block = chain.mineBlock([
+        Tx.contractCall('creature-racer-admin',
+                        'verify-signature',
+                        [types.buff(sigs.operatorSignature),
+                         types.buff(sigs.senderSignature),
+                         types.list([types.uint(500),
+                                     types.uint(531101)])],
+                        sender.address
+                       )
+      ]);
+      assertEquals(block.height, 3);
+      assertEquals(block.receipts.length, 1);
+      assertEquals(block.receipts[0].result, '(err u1002)');
+    }
 });
--- protocol/tests/utils/admin.ts
+++ protocol/tests/utils/admin.ts
@@ -2,19 +2,69 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 import { pubKeyfromPrivKey, makeRandomPrivKey,
          privateKeyToString,
-         getAddressFromPrivateKey } from 'https://esm.sh/@stacks/transactions';
+         getAddressFromPrivateKey,
+         signMessageHashRsv,
+         createStacksPrivateKey } from 'https://esm.sh/@stacks/transactions';
+//import keccak256 from 'https://esm.sh/keccak256';
+import { sha256 } from "https://denopkg.com/chiefbiiko/sha256@v1.0.0/mod.ts";
 
 export function setOperator(chain: Chain, deployer: Account, 
                      operator: Account) {
   let secretKey = 'd655b2523bcd65e34889725c73064feb17ceb796831c0e111ba1a552b0f31b3901';
-  let publicKey = pubKeyfromPrivKey(secretKey);
   let block = chain.mineBlock([
     Tx.contractCall('creature-racer-admin',
                     'set-operator', 
-                    [types.some(types.principal(operator.address)),
-                     types.some(types.buff(publicKey))],
+                    [types.some(types.principal(operator.address))],
                     deployer.address)
   ]);
   assertEquals(block.receipts.length, 1);
   assertEquals(block.receipts[0].result, '(ok true)');
+}
+
+
+function uint128toBytes(v: number) {
+  var rv = [];
+
+  for(var n = 0; n < 16; n++) {
+    rv[n] = v & 0xff;
+    v = v >> 8;
+  }
+  return rv;
+}
+
+function parseHexString(str) { 
+  var result = [];
+  var i = 0;
+  while (i < str.length) { 
+    result.push(parseInt(str.substring(i, i+2), 16));
+    i += 2;
+  }
+
+  return result;
+}
+
+type ArgSigs = {
+  operatorSignature: string,
+  senderSignature: string
+}
+
+export function makeSignatures(operatorPK: string,
+                               senderPK: string,
+                               ...args: number[]): ArgSigs {
+  // Start by computing argument checksum
+  var buff = [];
+  for(var i = 0; i < args.length; i++) {
+    buff = buff.concat(uint128toBytes(args[i]));
+  }
+
+  const argshash = sha256(buff);
+  // sign the hash with sender key
+  const ssig = signMessageHashRsv({ messageHash: argshash,
+                                    privateKey: createStacksPrivateKey(senderPK) });
+  const msg = parseHexString(ssig.data).concat(buff);
+  const msghash = sha256(msg);
+  const opsig = signMessageHashRsv({ messageHash: msghash,
+                                     privateKey: createStacksPrivateKey(operatorPK) });
+  return { senderSignature: parseHexString(ssig.data),
+           operatorSignature: parseHexString(opsig.data) };
 }
