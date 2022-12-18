edfc6e6e8558a5d1641ccf8fba2360a5ed79d4b3
pki-acs <pki-acs@ex.com.pl>
2022-12-18 22:42:57 +0100

    added vue microstacks mint example

---
+++ examples/ustx-vue-mint-example/src/components/WalletConnectButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAccount, useAuth } from '@micro-stacks/vue';
+
+const { isSignedIn, isRequestPending, signOut, 
+        openAuthRequest } = $(useAuth());
+const { stxAddress } = $(useAccount());
+
+const onClick = () => (!isSignedIn ? openAuthRequest() : signOut());
+</script>
+
+<template>
+  <h1>{{ stxAddress ? stxAddress : 'No session active' }}</h1>
+  <button
+    type="button"
+    v-on:click="() => onClick()"
+  >
+    {{ isRequestPending ? 'pending...' : isSignedIn ? 'sign out' : 'connect stacks wallet' }}
+  </button>
+</template>
